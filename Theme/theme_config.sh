b1af6e582f28aa470373a9c85ab3f1f7feb3fb54
Frank Laércio <38151364+franklaercio@users.noreply.github.com>
2020-09-06 22:11:13 -0300

    Update theme_config

--- Theme/theme_config.sh
+++ Theme/theme_config.sh
@@ -7,6 +7,7 @@ git clone https://github.com/daniruiz/flat-remix
 git clone https://github.com/daniruiz/flat-remix-gtk
 git clone https://github.com/daniruiz/flat-remix-gnome
 cd flat-remix-gnome
+sudo apt install make
 make && sudo make install
 mkdir -p ~/.icons && mkdir ~/.themes
 cp -r flat-remix/Flat-Remix* ~/.icons/ && cp -r flat-remix-gtk/Flat-Remix-GTK* ~/.themes/
